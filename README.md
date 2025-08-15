96aa2fba3ae9780db9f63e3628ed0396e282fd85
voiceinthedark <darkrisingforce@gmail.com>
2025-08-15 12:32:59 +0300

    initial scaffolding
    style: initial default styling added
    docs(readme): initial readme file
    build(babel): added babel configs
    build(package): initiated default package with dependencies, added babel
    and jest
    
    scaffolding
    
    chore: clean modules, remove unwanted files. Clean appcontroller.js and
    index.js
    
    added types jest
    
    build(package): added @types/jest

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# WeatherJS
+# Battleship game
 
 ## About
 
-A weather application with vanilla javascript and css.
-
-## Roadmap
-
-- [x] Add Geolocation
-- [x] Add hourly forecast for the day
-- [x] Add hourly forecast for each day of the week
-- [x] Make the week's list items interactive
-- [ ] Make hour list auto scroll to current hour
-- [ ] Make the week's list backgrounds vary in color degrees
-
-## Assets
-
-- [icons8](https://icons8.com) - for some icons
-- [visual crossing](https://visualcrossing.com) - for the api and weather icons
-- [fontawesome](https://fontawesome.com) - for the font icon of github and the spinner
+A battleship game implementation in vanilla js 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import AppController from "./modules/appcontroller";
+import AppController from "./modules/appcontroller.js";
 import "./styles/styles.css";
 import "./styles/fontawesome.min.css";
 import "./styles/regular.min.css";
@@ -8,27 +8,7 @@ import "./styles/brands.min.css";
 const appContainer = document.getElementById("container");
 
 async function runApp() {
-  const app = new AppController(appContainer);
-  await app.initialize(); // Wait for the data to be fetched
-
-  const data = app.getData();
-  if (data) {
-    console.log("Fetched Data:", data);
-    // You can now safely use the data
-  } else {
-    console.log("Data could not be fetched or is not available.");
-    app.setEmptyResult();
-  }
-
-  app.setSearchBar();
-  app.setDayCard();
-  // console.log(app.getWeekData())
-  // console.log(app.getWeek())
-  //
-  app.setWeekList();
-  app.setupFooter();
-  // console.log(app.getHoursData())
-  console.log(app.getGroupedByHoursData());
+  
 }
 
 runApp();
