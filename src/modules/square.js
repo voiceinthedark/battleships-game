448a67fc6e3d2801b33e79e5e297585482181f8e
voiceinthedark <darkrisingforce@gmail.com>
2025-08-15 16:36:35 +0300

    Add square module
    feat: added square module to represent a coordinate on the game board
    test: added unit tests for the square module

---
+++ src/modules/square.js
@@ -0,0 +1,51 @@
+// square.js
+// @ts-check
+
+/**
+ * @class Square
+ * @classdesc Represents the coordinates of the square on the board
+ * */
+class Square {
+  #x
+  #y
+  #hit
+
+  /**
+   * @constructor
+   * @param {number} [x=0] - the x coordinate on the board
+   * @param {number} [y=0] - the y coordinate on the board
+   * @param {boolean} [hit=false] - whether the coordinate is hit or not
+   * */
+  constructor(x = 0, y = 0, hit = false) {
+    this.#x = x
+    this.#y = y
+    this.#hit = hit
+  }
+
+  /**
+   * Return the x coordinate
+   * @returns {number}
+   * */
+  get x() {
+    return this.#x
+  }
+
+  /**
+   * Returns the y coordinate
+   * @returns {number}
+   * */
+  get y() {
+    return this.#y
+  }
+
+  /**
+   * Return whether the coordinate is hit or not
+   * @returns {boolean}
+   * */
+  isHit() {
+    return this.#hit
+  }
+}
+
+/** @module Square */
+export default Square;
