6a2105d7bef530e6c4700c9c198b7754bc5b763c
voiceinthedark <darkrisingforce@gmail.com>
2025-08-22 14:02:17 +0300

    feat: add modal controller

---
+++ src/modules/UI/modalController.js
@@ -0,0 +1,68 @@
+// modalcontroller.js
+// @ts-check
+
+/**
+ * @typedef Results
+ * @property {Object} time
+ * @property {string} time.name
+ * @property {string} time.value
+ * @property {object} ships
+ * @property {string} ships.name
+ * @property {number} ships.value
+ * @property {object} misses
+ * @property {string} misses.name
+ * @property {number} misses.value
+ * */
+
+import Player from "../player.js"
+import GameBoard from "../gameboard.js"
+import UIManager from "./uimanager.js"
+
+class ModalController {
+  #uimanager
+
+  /**
+   * @constructor 
+   * @param {UIManager} uimanager 
+   * */
+  constructor(uimanager) {
+    this.#uimanager = uimanager
+  }
+
+  /**
+   * method render to render the modal on the screen
+   * @param {Player} winner 
+   * @param {Results} results 
+   * @param {(event: Event) => void} handleClickCallback 
+   * @returns {HTMLElement} the modal-container element
+   * */
+  render(winner, results, handleClickCallback) {
+    // TODO render the modal on the screen
+    const modalContainer = document.createElement('div')
+    modalContainer.classList.add('.modal-container')
+
+    const nameSpan = this.#uimanager.addElement('span', modalContainer, 'modal-name')
+    nameSpan.textContent = `${winner.name} wins the game!`
+
+    const summaryTable = this.#uimanager.addElement('table', modalContainer, 'modal-summary')
+    for (const [key, categoryData] of Object.entries(results)) {
+      const trow = this.#uimanager.addElement('tr', summaryTable, 'summary-row')
+
+      const tdName = this.#uimanager.addElement('td', trow, 'summary-element')
+      tdName.textContent = categoryData.name
+
+      const tdValue = this.#uimanager.addElement('td', trow, 'summary-element')
+      tdValue.textContent = String(categoryData.value) // Ensure value is a string for textContent
+    }
+
+    const keySpan = this.#uimanager.addElement('span', modalContainer, 'modal-key')
+    keySpan.textContent = 'Press any key to close...'
+
+    //TODO handleClickCallback
+    modalContainer.addEventListener('click', handleClickCallback)
+
+    return modalContainer
+  }
+}
+
+export default ModalController
